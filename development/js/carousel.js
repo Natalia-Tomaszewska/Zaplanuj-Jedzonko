7b02de9b7013c93444f041f6a3babe34f1da93c3
Jakub Klimczak <jakub.klimczak.91@gmail.com>
2020-10-13 09:50:54 +0200

    Partly done. JS to go.

---
+++ development/js/carousel.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    {
+        const track = document.querySelector(".carousel__track");
+        const slides = document.querySelectorAll(".carousel__slide");
+        const next = document.querySelector(".carousel__next");
+        const prev = document.querySelector(".carousel__prev");
+        const dotsNav = document.querySelector(".carousel__nav");
+        const dots = document.querySelectorAll(".carousel__nav-btn");
+
+        const slideSize = slides[0].
+        
+        
+    }
+});
