2f3c812de89365a140aa14f41eedd11e94cc0c35
Jakub Klimczak <jakub.klimczak.91@gmail.com>
2020-10-14 08:29:55 +0200

    Slider finished.

--- development/js/carousel.js
+++ development/js/carousel.js
@@ -1,15 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    {
-        const track = document.querySelector(".carousel__track");
-        const slides = document.querySelectorAll(".carousel__slide");
-        const next = document.querySelector(".carousel__next");
-        const prev = document.querySelector(".carousel__prev");
-        const dotsNav = document.querySelector(".carousel__nav");
-        const dots = document.querySelectorAll(".carousel__nav-btn");
+    const track = document.querySelector(".carousel__track");
+    const slides = document.querySelectorAll(".carousel__slide");
+    const next = document.querySelector(".carousel__next");
+    const prev = document.querySelector(".carousel__prev");
 
-        const slideSize = slides[0].
-        
-        
+    const moveSlide = (currentSlide, targetSlide) => {
+        currentSlide.classList.remove("current-slide");
+        targetSlide.classList.add("current-slide");
     }
+    // moving slides right after clicking next button
+    next.addEventListener("click", () => {
+        const currentSlide = track.querySelector(".current-slide");
+        const nextSlide = currentSlide.nextElementSibling;
+        const firstSlide = track.firstElementChild;
+        if (!nextSlide) {
+            moveSlide(currentSlide, firstSlide);
+        } else {
+            moveSlide(currentSlide, nextSlide);
+        }
+    });
+    // moving slides left after clicking prev button
+    prev.addEventListener("click", () => {
+        const currentSlide = track.querySelector(".current-slide");
+        const prevSlide = currentSlide.previousElementSibling;
+        const lastSlide = track.lastElementChild;
+        if (!prevSlide) {
+            moveSlide(currentSlide, lastSlide);
+        } else {
+            moveSlide(currentSlide, prevSlide);
+        }
+    });
 });
